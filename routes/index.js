373edc23a520642b7d60154af8de8489a5155c19
ShabadLamba <shabad.l@imimobile.com>
2020-04-14 00:36:40 +0530

    added new routes

--- routes/index.js
+++ routes/index.js
@@ -2,15 +2,18 @@ var express = require('express');
 var router = express.Router();
 const cheerio = require('cheerio');
 
-/* GET home page. */
-router.post('/api/v1/cheerio', function(req, res, next) {
-  if(!req.body) {
+router.get('cheerio/api/v1/heartbeat', function (req, res, next) {
+  res.json({ "success": true });
+});
+
+router.post('cheerio/api/v1/extract', function (req, res, next) {
+  if (!req.body) {
     res.json("body is empty")
   }
-  if(!req.body.html) {
+  if (!req.body.html) {
     res.json("html is empty");
   }
-  if(!req.body.query_selector) {
+  if (!req.body.query_selector) {
     res.json("invalid selector");
   }
   var html = req.body.html;
@@ -18,11 +21,11 @@ router.post('/api/v1/cheerio', function(req, res, next) {
   const $ = cheerio.load(html);
   const data = [];
 
-  $(query_selector).each(function(i, elem) {
-      data[i] = $(this).text();
+  $(query_selector).each(function (i, elem) {
+    data[i] = $(this).text();
   });
 
-  res.json({data: data});
+  res.json({ data: data });
 });
 
 module.exports = router;
